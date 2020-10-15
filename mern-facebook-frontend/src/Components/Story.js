91a9d565330a4ee7acb013ce570833eaf0980d9a
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-15 15:58:33 +0300

    backend init

--- mern-facebook-frontend/src/Components/Story.js
+++ mern-facebook-frontend/src/Components/Story.js
@@ -6,6 +6,7 @@ function Story({ image, profileSrc, title }) {
   return (
     <div className="story" style={{ backgroundImage: `url(${image})` }}>
       <Avatar src={profileSrc} className="story__avatar" />
+
       <h4>{title}</h4>
     </div>
   );
