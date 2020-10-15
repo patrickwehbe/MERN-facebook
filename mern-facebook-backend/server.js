6c61cce46341976b484b04e561e316c2dea9efd4
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-15 16:26:56 +0300

    db model + bug fix

--- mern-facebook-backend/server.js
+++ mern-facebook-backend/server.js
@@ -8,6 +8,8 @@ import bodyParser from "body-parser";
 import path from "path";
 import Pusher from "pusher";
 
+import mongoPosts from "./postModel.js";
+
 Grid.mongo = mongoose.mongo;
 
 //app config
@@ -20,9 +22,51 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //db configs
+const mongoURI =
+  "mongodb+srv://admin:LMd2Tq9XjegK1x6B@cluster0.jtcwe.mongodb.net/facebook-db?retryWrites=true&w=majority";
+
+const conn = mongoose.createConnection(mongoURI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let gfs;
+conn.once("open", () => {
+  console.log("DB Connected");
+
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("images");
+});
+
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      const filename = `image-${Date.now()}${path.extname(file.originalname)}`;
+      const fileInfo = {
+        filename: filename,
+        bucketName: "images",
+      };
+      resolve(fileInfo);
+    });
+  },
+});
+
+const upload = multer({ storage });
+
+mongoose.connect(mongoURI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //api routes
 app.get("/", (req, res) => res.status(200).send("Hello, World"));
+
+app.post("/upload/image", upload.single("file"), (req, res) => {
+  res.status(201).send(req.file);
+});
 
 //listen
 app.listen(port, () => console.log(`Listening on Localhost: ${port}`));
