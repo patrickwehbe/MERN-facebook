0160ffed14bfd399cdcf70fbdafda228fea9e7c5
Patrick Wehbe <pew02@mail.aub.edu>
2020-10-15 16:04:02 +0300

    server inti

--- mern-facebook-backend/server.js
+++ mern-facebook-backend/server.js
@@ -7,3 +7,22 @@ import Grid from "gridfs-stream";
 import bodyParser from "body-parser";
 import path from "path";
 import Pusher from "pusher";
+
+Grid.mongo = mongoose.mongo;
+
+//app config
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cors());
+
+//db configs
+
+//api routes
+app.get("/", (req, res) => res.status(200).send("Hello, World"));
+
+//listen
+app.listen(port, () => console.log(`Listening on Localhost: ${port}`));
